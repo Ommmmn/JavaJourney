d8f5aca460a50906f3c5de495f41b02fe43cb6f7
Om Natekar <natekarom1@gmail.com>
2025-06-04 22:31:40 +0530

    Conditional assignment

--- AssignmenConditional.java
+++ AssignmenConditional.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class AssignmenConditional{
     public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        // que 1
+
+        // if (num < 0) {
+        //     System.out.println("Negative Number");
+        // } else if (num == 0) {
+        //     System.out.println("Zero");
+        // } else {
+        //     System.out.println("Positive Number");
+        // } 
+
+
+        // que 2
+
+        
+        
+
+
 
         
     }
