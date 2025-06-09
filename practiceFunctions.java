8e0ca17ba11528f5312d12f3fc1129e722caddfd
Om Natekar <natekarom1@gmail.com>
2025-06-09 09:54:54 +0530

    Your message

---
+++ practiceFunctions.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+public class practiceFunctions {
+
+    // public static void  printHelloWorld(){
+    //     System.out.println("Hello World");
+    //     System.out.println("Hello World");
+    // }
+
+    // public static int calculateSum(int num1, int num2) { //parameter or formal paramameters
+        
+    //     int sum = num1 + num2;
+    //     return sum; 
+
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+        
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
+    //     int sum = calculateSum(a, b); //arguements
+    //     System.out.println("The sum is: " + sum);
+    // } 
+
+
+    // public static void swap(int a , int b) {
+        
+    //     int temp = a;
+    //     a= b;
+    //     b = temp;
+    //     System.out.println("a =" + a);
+    //     System.out.println("b =" + b);
+    // }
+
+
+    // public static void main(String[] args) {
+        
+    //     int a = 10;
+    //     int b = 20;
+    //     swap(a, b);
+
+        
+    // }
+    // product of two numbers
+
+    public static int multiply (int a, int b) {
+
+        int product = a * b;
+        return product;
+    }
+
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = 10;
+        int result =multiply(a,b);
+
+        System.out.println("a * b: " + result);
+
+    }
+
+
+
+
+
+
+
+
+
+    
+}
+
+
